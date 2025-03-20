ac309bb13d51c5583c6e48d6cac9e008d9ce95c8
Ismail Kerem Gul <ismail.gul@roofstacks.com>
2025-03-20 15:25:49 +0300

    created homeScreen

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -9,22 +9,41 @@ export const AppBar = ({text}: AppBarPropse) => {
   return (
     <View style={styles.appBar}>
       <View style={styles.leading}>
-        <MaterialIcons name="house" color="#ff0000" size={20} />
+        <MaterialIcons
+          name="house"
+          color="#ff0000"
+          style={styles.leading}
+          size={35}
+        />
       </View>
-      <Text style={{backgroundColor: 'white'}}>{text} </Text>
+      <View style={styles.textArea}>
+        <Text style={styles.textStyle}>{text} </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   appBar: {
-    backgroundColor: 'blue',
-    width: 100,
-    height: 30,
+    backgroundColor: 'dodgerblue',
+    width: '100%',
+    height: 45,
+    flexDirection: 'row',
   },
   leading: {
-    width: 30,
-    height: 30,
+    width: 60,
+    height: 40,
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 700,
+  },
+  textArea: {
+    width: 250,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
---
+++ src/screen/axios.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function AxiosPage() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>axios</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
